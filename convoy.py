52b328d6d54f4a5e42bce9d08d85dda8e2ed6a21
Marat Matourin <59852784+Devloppa@users.noreply.github.com>
2022-11-28 20:58:07 +0100

    Refactor code

--- convoy.py
+++ convoy.py
@@ -13,22 +13,10 @@ class MyTable:
         self.df = None
         self.rows = None
         self.cols = None
-        self.csv_file_name = None
-        self.df = None
         self.num_rect_cells = 0
-        self.rect_csv_name = None
-        self.output_file_name = None
-        self.db_name = None
-        self.csv = None
-        self.db_lst = []
         self.conn = None
         self.cursor = None
         self.num_of_insertions = 0
-        self.num_of_records = None
-        self.json_file_name = None
-        self.df_db = None
-        self.json_dict = {}
-        self.json_length = None
 
         self.define_file_type()
         self.process_file()
@@ -67,28 +55,7 @@ class MyTable:
             self.write_json_to_disk()
             self.print_num_vehicles()
 
-        elif self.file_type == "CHECKED_CSV":
-            self.csv_file_name = self.file_name
-            self.rect_csv_name = self.file_name
-            self.convert_csv_to_df()
-            self.define_db_name()
-            self.connect_to_db()
-            self.create_table()
-            self.persist_to_db()
-            self.print_db_insertions()
-            self.define_json_name()
-            self.write_json_to_disk()
-            self.print_num_vehicles()
-
-        elif self.file_type == "S3DB":
-            self.db_name = self.file_name
-            self.connect_to_db()
-            self.define_json_name()
-            self.write_json_to_disk()
-            self.print_num_vehicles()
-
         elif self.file_type == "CSV":
-            self.csv_file_name = self.file_name
             self.convert_csv_to_df()
             self.get_df_shape()
             self.rectify_csv()
@@ -104,24 +71,44 @@ class MyTable:
             self.write_json_to_disk()
             self.print_num_vehicles()
 
+        elif self.file_type == "CHECKED_CSV":
+            self.convert_csv_to_df()
+            self.define_db_name()
+            self.connect_to_db()
+            self.create_table()
+            self.persist_to_db()
+            self.print_db_insertions()
+            self.define_json_name()
+            self.write_json_to_disk()
+            self.get_df_shape()
+            self.print_num_vehicles()
+
+        elif self.file_type == "S3DB":
+            self.connect_to_db()
+            self.define_json_name()
+            self.write_json_to_disk()
+            self.get_df_shape()
+            self.print_num_vehicles()
+
+
     def convert_xlsx_to_df(self):
         self.df = pd.read_excel(f"{self.file_name}", sheet_name="Vehicles", dtype=str)
-
-    def define_csv_name(self):
-        self.csv_file_name = self.file_name.replace(".xlsx", ".csv")
-
-    def write_csv_to_disk(self):
-        self.df.to_csv(f"{self.csv_file_name}", index=None)
-
-    def print_xlsx_rows(self):
-        print(f"{self.rows} {'lines were' if self.rows > 1 else 'line was'} added to {self.csv_file_name}")
-
-    def convert_csv_to_df(self):
-        self.df = pd.read_csv(self.csv_file_name)
 
     def get_df_shape(self):
         self.rows = self.df.shape[0]
         self.cols = self.df.shape[1]
+
+    def define_csv_name(self):
+        self.file_name = self.file_name.replace(".xlsx", ".csv")
+
+    def write_csv_to_disk(self):
+        self.df.to_csv(f"{self.file_name}", index=None)
+
+    def print_xlsx_rows(self):
+        print(f"{self.rows} {'lines were' if self.rows > 1 else 'line was'} added to {self.file_name}")
+
+    def convert_csv_to_df(self):
+        self.df = pd.read_csv(self.file_name)
 
     def rectify_csv(self):
         for row in range(self.rows):
@@ -132,21 +119,21 @@ class MyTable:
                     self.num_rect_cells += 1
 
     def define_rect_csv_name(self):
-        self.rect_csv_name = re.sub(r"\.csv", "[CHECKED].csv", self.csv_file_name)
+        self.file_name = re.sub(r"\.csv", "[CHECKED].csv", self.file_name)
 
     def write_rect_to_disk(self):
-        self.df.to_csv(f"{self.rect_csv_name}", index=None)
+        self.df.to_csv(f"{self.file_name}", index=None)
 
     def print_num_rect_cells(self):
         num = self.num_rect_cells
         if num > 0:
-            print(f"{num} {'cells were corrected' if num > 1 else 'cell was corrected'} in {self.rect_csv_name}")
+            print(f"{num} {'cells were corrected' if num > 1 else 'cell was corrected'} in {self.file_name}")
 
     def define_db_name(self):
-        self.db_name = re.sub(r"\[CHECKED]\.csv", ".s3db", self.rect_csv_name)
+        self.file_name = re.sub(r"\[CHECKED]\.csv", ".s3db", self.file_name)
 
     def connect_to_db(self):
-        self.conn = sqlite3.connect(self.db_name)
+        self.conn = sqlite3.connect(self.file_name)
         self.cursor = self.conn.cursor()
 
     def create_table(self):
@@ -159,28 +146,26 @@ class MyTable:
                             """)
 
     def persist_to_db(self):
-        for vehicle_id, engine_capacity, fuel_consumption, maximum_load in self.df.values:
-            self.cursor.execute(f"INSERT INTO convoy VALUES ({vehicle_id}, {engine_capacity}, {fuel_consumption}, {maximum_load});")
+        for veh_id, eng_cap, fuel_consum, max_load in self.df.values:
+            self.cursor.execute(f"INSERT INTO convoy VALUES ({veh_id}, {eng_cap}, {fuel_consum}, {max_load});")
             self.num_of_insertions += 1
         self.conn.commit()
 
     def print_db_insertions(self):
         ins = self.num_of_insertions
-        print(f"{ins} {'records were' if ins > 1 else 'record was'} inserted into {self.db_name}")
+        print(f"{ins} {'records were' if ins > 1 else 'record was'} inserted into {self.file_name}")
 
     def define_json_name(self):
-        self.json_file_name = re.sub(r"\.s3db", ".json", self.db_name)
+        self.file_name = re.sub(r"\.s3db", ".json", self.file_name)
 
     def write_json_to_disk(self):
-        self.df_db = pd.DataFrame(pd.read_sql("SELECT * FROM convoy", self.conn))
-        self.json_dict["convoy"] = self.df_db.to_dict(orient="records")
+        self.df = pd.DataFrame(pd.read_sql("SELECT * FROM convoy", self.conn))
 
-        with open(f"{self.json_file_name}", mode="w") as json_file:
-            json.dump(self.json_dict, json_file, indent=4)
+        with open(f"{self.file_name}", mode="w") as json_file:
+            json.dump({"convoy": self.df.to_dict(orient="records")}, json_file, indent=4)
 
     def print_num_vehicles(self):
-        length = len(self.json_dict["convoy"])
-        print(f"{length} {'vehicles were' if length > 1 else 'vehicle was'} saved into {self.json_file_name}")
+        print(f"{self.rows} {'vehicles were' if self.rows > 1 else 'vehicle was'} saved into {self.file_name}")
 
 
 MyTable()
